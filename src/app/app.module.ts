ad6d5c76a239b111b494b7e26620c01b320f9ad4
EliasElisio192388 <sailenamber@gmail.com>
2023-09-09 11:13:16 -0300

    job resolve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './pages/footer/footer.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { ContatoComponent } from './pages/contato/contato.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    MenuComponent,
+    ContatoComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule
